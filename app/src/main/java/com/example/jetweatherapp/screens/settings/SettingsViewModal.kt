84f7aab2f7ec6459a552eeef9bc7fe1a449f5e8e
SAIF879 <saifahmed9308@gmail.com>
2023-04-27 13:02:11 +0530

    SettingsViewModal.kt completed , recheck nec ?? maybe

--- app/src/main/java/com/example/jetweatherapp/screens/settings/SettingsViewModal.kt
+++ app/src/main/java/com/example/jetweatherapp/screens/settings/SettingsViewModal.kt
@@ -1,4 +1,45 @@
 package com.example.jetweatherapp.screens.settings
 
-class SettingsViewModal {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.jetweatherapp.repository.WeatherDbRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class SettingsViewModal @Inject constructor(private  val repository: WeatherDbRepository) : ViewModel() {
+
+
+    private  val _unitList = MutableStateFlow<List<Unit>>(emptyList())
+    val unitList = _unitList.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            repository.getUnits().distinctUntilChanged().collect{
+                if(it.isNullOrEmpty()){
+                    Log.d("SET", ":emtpy settings ")
+                }
+                else {
+                    _unitList.value = it
+                }
+            }
+        }
+    }
+
+
+    suspend fun insertUnit(unit : Unit) = viewModelScope.launch { repository.insertUnit(unit = unit)}
+
+    suspend fun updateUnit(unit : Unit) = viewModelScope.launch { repository.updateUnit(unit = unit)}
+
+    suspend fun  deleteAllUnit() = viewModelScope.launch {repository.deleteAllUnit()  }
+
+    suspend fun deleteUnit(unit : Unit) = viewModelScope.launch { repository.deleteUnit(unit = unit ) }
+
+
+
+
 }
